dbb6bc08db87707d57902f364a647ba0ba615558
Zach Dunton <fenfir@gmail.com>
2023-05-07 18:55:33 +0200

    Add display loop

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from colorama import Style
 from random import randrange
 from typing import Optional
 from fastapi import FastAPI
-import time
+import asyncio
 
 colorama_init()
 
@@ -15,6 +15,7 @@ MAX_ITER = 10
 ## End for testing
 
 NUM_LEDS = 10
+running = False
 
 output = collections.deque([0 for i in range(NUM_LEDS)])
 
@@ -28,8 +29,6 @@ output_colors = [
     Fore.YELLOW,
 ]
 
-
-   
 def display_output(value: int):
      # function to display the output based on the input value
     output_string = ""
@@ -41,29 +40,24 @@ def display_output(value: int):
 
     print(output_string)
 
+def update_light(value: int):
+    output[0] = value
 
 def check_input():
     i = randrange(10)
     return normalize_input(i)
 
-# to-do
+# TODO
 def normalize_input(input):
     return input
 
 
-running = True
-while running:
-    i = check_input()
-    output[0] = i
+async def update_lights():
+    while running:
+        display_output(output)
 
-    display_output(output)
-
-    output.rotate(1)
-    time.sleep(1)
-    # loop_count += 1
-    # if loop_count > MAX_ITER:
-    #     running = False
-
+        output.rotate(1)
+        await asyncio.sleep(1)
 
 app = FastAPI()
 
@@ -71,7 +65,20 @@ app = FastAPI()
 def get_value(value: int):
     # function to handle the GET request and call display_output function
     if 0 <= value <= 256:
-        result = display_output(value)
+        result = update_light(value)
         return {"result": result}
     else:
         return {"error": "The value entered must be between 0 and 256."}
+
+@app.on_event("startup")
+async def startup_event():
+    print("Starting display loop...")
+    global running
+    running = True
+    asyncio.create_task(update_lights())
+
+@app.on_event("shutdown")
+def shutdown_event():
+    print("Shutting down...")
+    global running
+    running = False
